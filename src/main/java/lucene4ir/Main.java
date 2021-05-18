9d73a389825c375a6ba811b3ee8009e124f07e53
zEsteban22 <43429903+zEsteban22@users.noreply.github.com>
2021-05-18 05:44:16 -0600

    Basic interface

--- src/main/java/lucene4ir/Main.java
+++ src/main/java/lucene4ir/Main.java
@@ -1,9 +1,14 @@
 package lucene4ir;
 
+import view.View;
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello world");
+        SwingUtilities.invokeLater(()->new View());
+
     }
 }
--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -1,4 +1,17 @@
 package view;
 
-public class View {
+import javax.swing.*;
+
+public class View extends JFrame {
+    private JButton button1;
+    private JButton button2;
+    private JPanel _;
+
+    public View (){
+        super("Information retrieval with Lucene.");
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setContentPane(_);
+        setSize(300,300);
+    }
 }
